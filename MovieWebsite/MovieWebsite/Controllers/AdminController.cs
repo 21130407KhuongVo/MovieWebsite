55bd605c438420a5fdc6df4d12c3e91e2582f39a
Quan-2507 <21130495@st.hcmuaf.edu.vn>
2024-08-13 10:51:25 +0700

    re-update _Layout

---
+++ MovieWebsite/MovieWebsite/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieWebsite.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: /Admin/Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
