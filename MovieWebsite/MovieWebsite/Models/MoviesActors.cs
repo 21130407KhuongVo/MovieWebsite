c168cfc2d8ca6351abfe8b59199f7595af2d8a84
Nhan2108cute <21130460@st.hcmuaf.edu.vn>
2024-08-20 00:18:26 +0700

    Model get detail movie

---
+++ MovieWebsite/MovieWebsite/Models/MoviesActors.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieWebsite.Models
+{
+    public class MoviesActors
+    {
+        public Guid MoviesId { get; set; }
+        public Movie Movie { get; set; }
+
+        public Guid ActorsId { get; set; }
+        public Actor Actor { get; set; }
+    }
+}
