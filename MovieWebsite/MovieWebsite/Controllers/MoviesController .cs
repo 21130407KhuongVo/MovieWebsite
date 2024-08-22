5e8038fae66450c2190e1f686e78d098757205f4
Thiện Nhân Trần <thiennhanyahoo@gmail.com>
2024-08-23 04:10:21 +0700

    hienthimovie

---
+++ MovieWebsite/MovieWebsite/Controllers/MoviesController .cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MovieWebsite.Model.DomainModel;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace MovieWebsite.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ListMovies()
+
+        {
+            var model = new AdminViewModel();
+            List<Movies> movies = new List<Movies>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync($"{model.baseURL}/api/movie"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    movies = 
+                }
+            }
+
+            model.MovieList = movies;
+            // Implement your logic here
+            return View();
+        }
+    }
+    public class MoviesViewModel
+    {
+        public Movies Movies { get; set; }
+        public string baseURL = "https://localhost:7271";
+    }
+}
